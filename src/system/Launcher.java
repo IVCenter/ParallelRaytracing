2c40424f09641886770373074478d5e0207f8f88
Rex West <rexwest@gmail.com>
2014-03-31 19:16:23 -0700

    Added an onLaunch logging statement and ran the source to make sure the .gitignore was properly ignoring .class/.project/.classpath files

--- src/system/Launcher.java
+++ src/system/Launcher.java
@@ -1,5 +1,7 @@
 package system;
 
+import process.logging.Logger;
+
 public class Launcher {
 	
 	
@@ -22,6 +24,8 @@ public class Launcher {
 		//TODO: Parse the arguments
 		//TODO: Set configuration values
 		//TODO: Pass off control to the Delegate
+		
+		Logger.progress(-1, "Launching a Parallel Rendering Node with ID:[" + Configuration.getId() + "]...");
 		
 	}
 
