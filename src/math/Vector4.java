533655c5a15094f4f76f85370c58ff6022d29e7e
Rex West <rexwest@gmail.com>
2014-03-31 20:23:05 -0700

    Added methods to Vector4 to perform various Vector3 operations (cross, angle, dot, angle)

--- src/math/Vector4.java
+++ src/math/Vector4.java
@@ -33,4 +33,42 @@ public class Vector4 extends Vector4d {
 	{
 		super(v.x,v.y,v.z,v.w);
 	}
+	
+
+	/* *********************************************************************************************
+	 * Calculation Methods
+	 * *********************************************************************************************/
+	public Vector4 cross(Vector4 that)
+	{
+		Vector4 res = new Vector4();
+		res.x = this.y*that.z - this.z*that.y;
+		res.y = this.z*that.x - this.x*that.z;
+		res.z = this.x*that.y - this.y*that.x;
+		res.w = 0;
+		return res;
+	}
+	
+	public double angle(Vector4 that)
+	{
+		return 0.0;
+	}
+	
+	public Vector4 normalize3(Vector4 that)
+	{
+		double m = magnitude3();
+		x = x/m;
+		y = y/m;
+		z = z/m;
+		return this;
+	}
+	
+	public double magnitude3Sqrd()
+	{
+		return Math.pow(x, 2.0) + Math.pow(y, 2.0) + Math.pow(z, 2.0);
+	}
+	
+	public double magnitude3()
+	{
+		return Math.pow(magnitude3Sqrd(), 0.5);
+	}
 }
