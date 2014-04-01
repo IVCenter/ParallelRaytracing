61f97b8ad6c1f41465c20e2f403b7032ee7401bd
Rex West <rexwest@gmail.com>
2014-04-01 12:31:09 -0700

    Added a lazily evaluated line parser for file data importing

---
+++ src/file/LineParser.java
@@ -0,0 +1,119 @@
+package file;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+
+public class LineParser implements Iterable<String>{
+	
+	/*
+	 * A simple line parser that evaluates lazily to allow for reading of massive files
+	 */
+	
+	/* *********************************************************************************************
+	 * Instance Vars
+	 * *********************************************************************************************/
+	protected File file;
+	
+
+	/* *********************************************************************************************
+	 * Instance Vars
+	 * *********************************************************************************************/
+	public LineParser(File file)
+	{
+		this.file = file;
+	}
+
+	@Override
+	public Iterator<String> iterator()
+	{
+		return new LineIterator();
+	}
+	
+
+	/* *********************************************************************************************
+	 * Private Classes
+	 * *********************************************************************************************/
+	private class LineIterator implements Iterator<String>
+	{
+		/* *********************************************************************************************
+		 * Instance Vars
+		 * *********************************************************************************************/
+		private BufferedReader inReader;
+		private String currentLine = null;
+		private boolean closed = false;
+
+		
+		/* *********************************************************************************************
+		 * Constructors
+		 * *********************************************************************************************/
+		public LineIterator()
+		{
+			try 
+			{
+				InputStream is =  new FileInputStream(file);
+				inReader = new BufferedReader(new InputStreamReader(is));
+			} 
+			catch (Exception e) {
+				e.printStackTrace();
+				throw new RuntimeException("Unable to read file [" + file.getName() + "]");
+			}
+		}
+
+		
+		/* *********************************************************************************************
+		 * Iterator Overrides
+		 * *********************************************************************************************/
+		@Override
+		public boolean hasNext()
+		{
+			boolean hasNext = inReader != null && currentLine != null;
+			
+			//Fast return if we have a next
+			if(hasNext)
+				return true;
+			
+			//If we're out of lines, and have not yet closed the stream, close it
+			if(!closed && inReader != null) {
+				try {
+					inReader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				closed = true;
+			}
+			
+			return false;
+		}
+
+		@Override
+		public String next()
+		{
+			//If we don't have a valid reader, or are out of lines, return null
+			if(inReader == null || currentLine == null)
+				return null;
+			
+			String next = currentLine;
+			try {
+				currentLine = inReader.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+				currentLine = null;
+			}
+			
+			return next;
+		}
+
+		@Override
+		public void remove()
+		{
+			//Nope
+		}
+		
+	}
+
+}
---
+++ src/file/obj/ObjFileLoader.java
@@ -0,0 +1,5 @@
+package file.obj;
+
+public class ObjFileLoader {
+
+}
