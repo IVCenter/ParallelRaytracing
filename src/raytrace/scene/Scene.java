45d84923b1d930e057505e16d74dc88ff6cc5435
Rex West <rexwest@gmail.com>
2014-04-03 11:43:49 -0700

    A Scene is now a user configurable composite surface with essential tracing information like activeCamera, etc.

--- src/raytrace/scene/Scene.java
+++ src/raytrace/scene/Scene.java
@@ -1,9 +1,39 @@
 package raytrace.scene;
 
-public class Scene {
+import raytrace.camera.Camera;
+import raytrace.data.BakeData;
+import raytrace.surfaces.CompositeSurface;
+
+public class Scene extends CompositeSurface{
 	
 	/*
 	 * A scene to be rendered
 	 */
+	/* *********************************************************************************************
+	 * Instance Vars
+	 * *********************************************************************************************/
+	protected Camera activeCamera;
+	
+
+	/* *********************************************************************************************
+	 * Cosntructors
+	 * *********************************************************************************************/
+	public Scene()
+	{
+		//Empty Scene
+	}
+	
+	
+	
+
+
+	/* *********************************************************************************************
+	 * Surface Overrides
+	 * *********************************************************************************************/
+	@Override
+	public void bake(BakeData data) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
