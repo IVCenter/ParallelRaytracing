6e154e0860064265b8cc572d10a8cd35bb7a73a3
Rex West <rexwest@gmail.com>
2014-04-02 19:07:08 -0700

    Added a clear() method to BoundingBox (useful when updating)

--- src/raytrace/bounding/BoundingBox.java
+++ src/raytrace/bounding/BoundingBox.java
@@ -22,13 +22,30 @@ public class BoundingBox {
 	{
 		min = new Vector4();
 		max = new Vector4();
+		
+		clear();
 	}
 	
 	public BoundingBox(Vector4 min, Vector4 max)
 	{
 		this.min = min;
 		this.max = max;
+		
+		clear();
 	}
-	
+
+	/* *********************************************************************************************
+	 * Reset Method
+	 * *********************************************************************************************/
+	public void clear()
+	{
+		min.x = Double.POSITIVE_INFINITY;
+		min.y = Double.POSITIVE_INFINITY;
+		min.z = Double.POSITIVE_INFINITY;
+
+		max.x = Double.NEGATIVE_INFINITY;
+		max.y = Double.NEGATIVE_INFINITY;
+		max.z = Double.NEGATIVE_INFINITY;
+	}
 	
 }
