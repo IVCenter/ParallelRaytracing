dc090eae07dd461f1f79cd666313f6b2a19f3125
Rex West <rexwest@gmail.com>
2014-04-03 13:12:10 -0700

    Terminal Surfaces now mask all of the composite methods of CompositeSurface

--- src/raytrace/surfaces/TerminalSurface.java
+++ src/raytrace/surfaces/TerminalSurface.java
@@ -3,11 +3,44 @@ package raytrace.surfaces;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import raytrace.data.BakeData;
+import raytrace.data.IntersectionData;
+import raytrace.data.RayData;
+import raytrace.data.UpdateData;
+
 public abstract class TerminalSurface extends CompositeSurface {
 
 	/*
 	 * Masks the composite aspects of the Composite Surface
 	 */
+
+	/* *********************************************************************************************
+	 * Masking Surface Methods
+	 * *********************************************************************************************/
+	@Override
+	public IntersectionData intersects(RayData data)
+	{
+		//Mask
+		return null;
+	}
+	
+	@Override
+	public void update(UpdateData data)
+	{
+		//Mask
+	}
+	
+	@Override
+	public void bake(BakeData data)
+	{
+		//Mask
+	}
+
+	@Override
+	public void updateBoundingBox()
+	{
+		//Mask
+	}
 
 	/* *********************************************************************************************
 	 * Masking Composite Methods
