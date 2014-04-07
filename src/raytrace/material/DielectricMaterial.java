841b80b47c3309a7573a0218395e877feadec57c
Rex West <rexwest@gmail.com>
2014-04-07 10:06:25 -0700

    Implemented a basic (and slightly messy) dielectric material shader.  It looks slightly incorrect and needs work.

--- src/raytrace/material/DielectricMaterial.java
+++ src/raytrace/material/DielectricMaterial.java
@@ -16,17 +16,17 @@ public class DielectricMaterial extends Material{
 	/* *********************************************************************************************
 	 * Instance Vars
 	 * *********************************************************************************************/
-	protected Color color;
-	double reflectivePercent = 0.5;
+	protected Color tint;
+	double refractiveIndex = 1.0;
 	
 
 	/* *********************************************************************************************
 	 * Constructor
 	 * *********************************************************************************************/
-	public DielectricMaterial(/*TODO: Define parameter list*/)
+	public DielectricMaterial(Color tint, double refractiveIndex)
 	{
-		//this.color = color;
-		//this.reflectivePercent = reflectivePercent;
+		this.tint = tint;
+		this.refractiveIndex = refractiveIndex;
 	}
 	
 
@@ -36,7 +36,18 @@ public class DielectricMaterial extends Material{
 		Color shade = new Color(0x000000ff);
 		
 		Vector4 point = data.getIntersectionData().getPoint();
-		Vector4 normal = data.getIntersectionData().getNormal();
+		
+		//Setup the normal
+		//TODO: Find out why these arent normalized to begin with!
+		Vector4 normal = data.getIntersectionData().getNormal();//.normalize3();
+		double DdotN = normal.dot3(data.getRay().getDirection());
+		if(DdotN > 0.0) {
+			normal = normal.multiply3(-1.0);
+			DdotN *= -1.0;
+		}
+		
+		
+		
 		IlluminationData ildata;
 		
 		IntersectionData shadowData;
@@ -56,17 +67,68 @@ public class DielectricMaterial extends Material{
 			if(shadowData == null)
 				shade.add3M(diffuse(ildata.getColor(), normal, ildata.getDirection()));
 		}
+		
+		
+		//Pre-calculate some values for refraction
+		double incomingRefractiveIndex = data.getRefractiveIndex();
+		Vector4 rayDir = data.getRay().getDirection();
+		//DdotN = Math.abs(DdotN);
+		
+		//Calculate the refraction direction
+		double refractiveRatio = incomingRefractiveIndex / refractiveIndex;
+		Vector4 thetaSide = rayDir.subtract3(normal.multiply3(DdotN)).multiply3(refractiveRatio);
+		double phiDiscrim = 1.0 - (refractiveRatio * refractiveRatio) *
+							(1.0 - (DdotN * DdotN));
+		Vector4 phiSide = normal.multiply3(Math.sqrt(phiDiscrim));
+		
+		Vector4 refracDir = thetaSide.subtract3(phiSide);
+		
+		Color refractColor = new Color();
+		if(phiDiscrim > 0.0 && data.getRecursionDepth() < DO_NOT_EXCEED_RECURSION_LEVEL)
+		{
+			refractColor = recurse(data, point, refracDir, refractiveIndex);
+			//refractColor.print();
+		}
+		
+		
+		
+		
+		
+		//Calculate the reflective coefficient
+		double baseReflectiveCoeff = (refractiveIndex - 1.0) / (refractiveIndex + 1.0);
+		baseReflectiveCoeff *= baseReflectiveCoeff;
+		double reflectiveCoeff = baseReflectiveCoeff + (1.0 - baseReflectiveCoeff) * Math.pow(1.0 - Math.abs(DdotN), 5.0);
+		
+		//If there was no refraction, fix reflective coeff
+		if(phiDiscrim <= 0.0)
+			reflectiveCoeff = 1.0;
+
+		//System.out.println("5pow: " + (Math.pow(1.0 - Math.abs(DdotN), 5.0)));
+		
 
 		//If reflective, go divin'
 		Color rflectColor = new Color();
-		if(reflectivePercent != 0.0 && data.getRecursionDepth() < DO_NOT_EXCEED_RECURSION_LEVEL)
+		if(reflectiveCoeff != 0.0 && data.getRecursionDepth() < DO_NOT_EXCEED_RECURSION_LEVEL)
 		{
 			rflectColor = reflect(data, point, normal);
+			//rflectColor.print();
 		}
 		
-		Color diffuseColor = color.multiply3(shade).multiply3(1.0 - reflectivePercent);
-		Color reflectiveColor = rflectColor.multiply3(reflectivePercent);
-		return diffuseColor.add3(reflectiveColor);
+		
+		//TODO: How does mix color
+		//TODO: Beers law on tint!
+		Color refractiveColor = refractColor.multiply3(tint).multiply3(1.0 - reflectiveCoeff);
+		Color reflectiveColor = rflectColor.multiply3(reflectiveCoeff);
+		
+		//System.out.println("RefractColor: ");
+		//refractColor.print();
+		//System.out.println("RefractCoeff: " + (1.0 - reflectiveCoeff));
+		//System.out.println("RefractiveColor: ");
+		//refractiveColor.print();
+		
+		
+		//return refractColor;
+		return refractiveColor.add3(reflectiveColor);
 	}
 
 }
