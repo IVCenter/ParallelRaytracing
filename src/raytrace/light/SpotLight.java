f041f20a6d30ab554758621cfb35d042a12bcff4
Rex West <rexwest@gmail.com>
2014-04-08 03:17:03 -0700

    Added a placeholder implementation for SpotLight

--- src/raytrace/light/SpotLight.java
+++ src/raytrace/light/SpotLight.java
@@ -1,5 +1,22 @@
 package raytrace.light;
 
+import process.logging.Logger;
+import math.Vector4;
+import raytrace.data.IlluminationData;
+
 public class SpotLight extends Light {
+
+	/*
+	 * A simple Spot Light class
+	 */
+	@Override
+	public IlluminationData illuminate(Vector4 point)
+	{
+		//TODO: Implement spot light
+		
+		Logger.warning(-1, "SpotLight is currently not supported. TODO");
+		
+		return null;
+	}
 
 }
