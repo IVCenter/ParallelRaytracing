b78bd94bf3e728d955724033f681199bd31b7a9a
Rex West <rexwest@gmail.com>
2014-04-09 22:12:39 -0700

    Added an interface and an implementation class for message listening: MessageListener and NetworkMessageListener

---
+++ src/network/MessageListener.java
@@ -0,0 +1,9 @@
+package network;
+
+public interface MessageListener {
+	
+	/*
+	 * A simple message listener interface
+	 */
+
+}
---
+++ src/network/NetworkMessageListener.java
@@ -0,0 +1,17 @@
+package network;
+
+public class NetworkMessageListener implements MessageListener {
+	
+	/*
+	 * A network based message listener
+	 */
+	/*
+	 * TODO:
+	 * 
+	 * 		-Open a socket
+	 * 		-Use a executor service and a worker class to handle requests on the socket
+	 * 			-Parse the message from the request
+	 * 			-Send it to a given handler
+	 */
+
+}
