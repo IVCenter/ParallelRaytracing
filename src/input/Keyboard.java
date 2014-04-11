ce106a90d7800840381e98e26b53144192f12f7a
Rex West <rexwest@gmail.com>
2014-04-11 00:22:09 -0700

    Added a Keyboard base class that manages key events from the user.

--- src/input/Keyboard.java
+++ src/input/Keyboard.java
@@ -1,5 +1,71 @@
 package input;
 
-public class Keyboard {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.HashMap;
 
+import process.logging.Logger;
+
+public abstract class Keyboard implements KeyListener {
+	
+	/*
+	 * A listener for keyboard events
+	 */
+
+	/* *********************************************************************************************
+	 * Instance Vars
+	 * *********************************************************************************************/
+	protected HashMap<Integer, Boolean> keyDown;
+
+	
+	/* *********************************************************************************************
+	 * Constructor
+	 * *********************************************************************************************/
+	public Keyboard()
+	{
+		keyDown = new HashMap<Integer, Boolean>();
+	}
+
+
+	/* *********************************************************************************************
+	 * Keyboard Event Listener Overrides
+	 * *********************************************************************************************/
+	@Override
+	public void keyTyped(KeyEvent e)
+	{
+		Logger.progress(-7, "Revceived a keyTyped() call for Key[" + e.getKeyChar() + "].");
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e)
+	{
+		Logger.progress(-7, "Revceived a keyPressed() call for Key[" + e.getKeyChar() + "].");
+		
+		keyDown.put(e.getKeyCode(), true);
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e)
+	{
+		Logger.progress(-7, "Revceived a keyReleased() call for Key[" + e.getKeyChar() + "].");
+
+		keyDown.put(e.getKeyCode(), false);
+	}
+	
+
+	/* *********************************************************************************************
+	 * Key Query Methods
+	 * *********************************************************************************************/
+	public boolean keyIsDown(int keyCode)
+	{
+		Boolean down = keyDown.get(keyCode);
+		if(down == null)
+			return false;
+		return down;
+	}
+	
+	public boolean keyIsUp(int keyCode)
+	{
+		return !keyIsDown(keyCode);
+	}
 }
