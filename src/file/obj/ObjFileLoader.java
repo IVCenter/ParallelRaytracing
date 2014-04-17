3592327a63d819ce628a4bc62ce8840a8abb28fb
Rex West <rexwest@gmail.com>
2014-04-16 20:43:05 -0700

    Implemented the top level programmable parser logic for loading an .obj file

--- src/file/obj/ObjFileLoader.java
+++ src/file/obj/ObjFileLoader.java
@@ -3,7 +3,15 @@ package file.obj;
 import java.io.File;
 import java.util.HashMap;
 
+import process.logging.Logger;
+
+import file.LineParser;
 import file.StringParser;
+import file.obj.parse.FaceParser;
+import file.obj.parse.NormalParser;
+import file.obj.parse.ObjectParser;
+import file.obj.parse.TexCoordParser;
+import file.obj.parse.VertexParser;
 
 public class ObjFileLoader {
 	
@@ -20,7 +28,7 @@ public class ObjFileLoader {
 	/* *********************************************************************************************
 	 * Static Parsers
 	 * *********************************************************************************************/
-	protected HashMap<String, StringParser<ObjModelData>> parsers;
+	protected static HashMap<String, StringParser<ObjModelData>> parsers;
 	
 
 	/* *********************************************************************************************
@@ -28,7 +36,14 @@ public class ObjFileLoader {
 	 * *********************************************************************************************/
 	static
 	{
-		//
+		parsers = new HashMap<String, StringParser<ObjModelData>>();
+
+		(new ObjectParser()).addTo(parsers);
+		(new VertexParser()).addTo(parsers);
+		(new NormalParser()).addTo(parsers);
+		(new TexCoordParser()).addTo(parsers);
+		(new FaceParser()).addTo(parsers);
+		//TODO: The rest
 	}
 	
 
@@ -38,7 +53,45 @@ public class ObjFileLoader {
 	public static ObjModelData load(String fileName) { return load(new File(fileName)); }
 	public static ObjModelData load(File file)
 	{
-		//
-		return null;
+		//Make sure the file exists
+		if(!file.exists()) {
+			Logger.warning(-9, "ObjFileLoader: The specified file does not exist.");
+			return null;
+		}
+		
+		//Create a new Object Model Data object
+		ObjModelData data = new ObjModelData();
+		
+		//Iterate the lines in the given file
+		String lineKey;
+		StringParser<ObjModelData> parser;
+		int firstSpaceIndex = -1;
+		for(String line : new LineParser(file))
+		{
+			firstSpaceIndex = line.indexOf(" ");
+			
+			if(line.length() == 0 || firstSpaceIndex < 0) {
+				//TODO: Do something 
+				continue;
+			}
+			
+			//Get the line key
+			lineKey = line.substring(0, firstSpaceIndex).trim();
+			
+			//Get the associated parser
+			parser = parsers.get(lineKey);
+			
+			//If we dont have a parser for this line, continue;
+			if(parser == null) {
+				Logger.warning(-8, "ObjFileLoader.load(): Encountered a line key [" + lineKey + "] that does not have" +
+						" and associated parser.");
+				continue;
+			}
+			
+			//Parse the line into the model data
+			parser.parse(line, data);
+		}
+		
+		return data;
 	}
 }
