6d596011e235ad5af55c52960fed0f49597d6ad8
Rex West <rexwest@gmail.com>
2014-04-16 20:44:49 -0700

    Modified launch settings.

--- src/system/Launcher.java
+++ src/system/Launcher.java
@@ -25,10 +25,10 @@ public class Launcher {
 		
 		//TODO: Parse the arguments
 		//TODO: Set configuration values
-		Configuration.setScreenWidth(1368);
-		Configuration.setScreenHeight(752);
-		//Configuration.setScreenWidth(800);
-		//Configuration.setScreenHeight(600);
+		//Configuration.setScreenWidth(1368);
+		//Configuration.setScreenHeight(752);
+		Configuration.setScreenWidth(800);
+		Configuration.setScreenHeight(600);
 		Configuration.setDrawToScreen(true);
 		Configuration.setClock(true);
 		Configuration.setLeaf(true);
@@ -38,7 +38,7 @@ public class Launcher {
 		
 		
 		//Load the scene (TODO: Parse args for scene choice)
-		String sceneKey = Constants.SceneKeys.TEST2;
+		String sceneKey = Constants.SceneKeys.TEST3;
 		SceneLoader sceneLoader = new SceneLoader();
 		Configuration.setMasterScene(sceneLoader.load(sceneKey));
 		
