685da827c62303e4fcc24129581aa5ebe739e7c8
Rex West <rexwest@gmail.com>
2014-04-17 00:11:09 -0700

    Added a CompositeObjectSurface class for managing obj file models that are created from several objects

---
+++ src/raytrace/geometry/meshes/CompositeObjectSurface.java
@@ -0,0 +1,16 @@
+package raytrace.geometry.meshes;
+
+import raytrace.surfaces.CompositeSurface;
+
+public class CompositeObjectSurface extends CompositeSurface {
+	
+	/*
+	 * A composite collection of objects as loaded from an .obj file
+	 */
+	/*
+	 * For each object, create an ObjectSurface
+	 * Create an AABVH for all objects
+	 * Add AABVH as this child
+	 */
+
+}
