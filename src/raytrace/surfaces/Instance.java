b10a682e73dc0532ad3c27f6397a40a09c5d3a23
Rex West <rexwest@gmail.com>
2014-04-17 15:01:51 -0700

    Modiefied Instance to support recursive setting of its children's materials.

--- src/raytrace/surfaces/Instance.java
+++ src/raytrace/surfaces/Instance.java
@@ -1,9 +1,20 @@
 package raytrace.surfaces;
 
+import raytrace.material.Material;
+
 public class Instance extends MatrixTransformSurface {
 	
 	/*
 	 * A convenient renaming ;)
 	 */
 
+	/* *********************************************************************************************
+	 * Overrides
+	 * *********************************************************************************************/
+	@Override
+	public void setMaterial(Material material)
+	{
+		for(CompositeSurface cs : this)
+			cs.setMaterial(material);
+	}
 }
