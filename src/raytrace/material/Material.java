dd4ca3557c205b3e0af69c3aa8b2d0b9e120e1fd
Rex West <rexwest@gmail.com>
2014-04-16 20:25:57 -0700

    Removed shadow calculation methods from Material.

--- src/raytrace/material/Material.java
+++ src/raytrace/material/Material.java
@@ -6,7 +6,6 @@ import raytrace.color.Color;
 import raytrace.data.IntersectionData;
 import raytrace.data.RayData;
 import raytrace.data.ShadingData;
-import raytrace.scene.Scene;
 
 public abstract class Material {
 	
@@ -46,27 +45,6 @@ public abstract class Material {
 		if(dot >= 0.0)
 			return Color.black();
 		return light.multiply3( dot * -1.0 );
-	}
-	
-	protected IntersectionData shadowed(Scene scene, Ray ray, double distanceToLight)
-	{
-		RayData rdata = new RayData();
-		rdata.setRay(ray);
-		rdata.setTStart(RECURSIVE_EPSILON);
-		IntersectionData idata = scene.intersects(rdata);
-		
-		//If no intersection, not shadowed
-		if(idata == null)
-			return null;
-		
-		//If the light is infinitely far, and we hit something, in shadow
-		if(distanceToLight == Double.MAX_VALUE)
-			return idata;
-		
-		if(idata.getDistance() < distanceToLight)
-			return idata;
-		
-		return null;
 	}
 	
 	protected Color reflect(ShadingData data, Vector4 point, Vector4 normal, double refractiveIndex)
