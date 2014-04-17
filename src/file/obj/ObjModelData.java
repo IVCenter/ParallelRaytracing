6872d7a0de3a977bf4f147c022134419dbb76398
Rex West <rexwest@gmail.com>
2014-04-16 20:43:28 -0700

    Added internal storage classes to ObjModelData

--- src/file/obj/ObjModelData.java
+++ src/file/obj/ObjModelData.java
@@ -6,10 +6,14 @@ public class ObjModelData {
 	
 	/*
 	 * A storage object for data loaded from an .obj file
+	 * Note: This is a stateful object under mutation.  
+	 * 		The add() methods will change behavior based on the internal state
+	 * In contrast,
+	 * 		The get() methods will always return deterministically
 	 */
 
 	/* *********************************************************************************************
-	 * Instnace Vars
+	 * Instance Vars
 	 * *********************************************************************************************/
 	protected ArrayList<Object> vertices;//TODO
 	
@@ -21,6 +25,28 @@ public class ObjModelData {
 	{
 		
 	}
-	
 
+	
+	/* *********************************************************************************************
+	 * Private Classes
+	 * *********************************************************************************************/
+	public static class Object {
+		
+	}
+	
+	public static class Vertex {
+		
+	}
+	
+	public static class Normal {
+		
+	}
+	
+	public static class TexCoord {
+		
+	}
+	
+	public static class Face {
+		
+	}
 }
