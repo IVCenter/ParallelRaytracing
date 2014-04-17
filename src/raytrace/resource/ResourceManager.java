17fa71466604daadac6b2e7b211caea54761436d
Rex West <rexwest@gmail.com>
2014-04-16 20:38:32 -0700

    Added a ResourceManager for automatically loading files and generating instances of pre-loaded resources.

---
+++ src/raytrace/resource/ResourceManager.java
@@ -0,0 +1,110 @@
+package raytrace.resource;
+
+import java.util.HashMap;
+
+import file.obj.ObjFileLoader;
+import file.obj.ObjModelData;
+
+import raytrace.geometry.meshes.Cube;
+import raytrace.geometry.meshes.ObjectMeshSurface;
+import raytrace.surfaces.CompositeSurface;
+import raytrace.surfaces.Instance;
+
+public class ResourceManager {
+	
+	/*
+	 * A resource manager for loading models, geometry, etc.
+	 */
+
+	/* *********************************************************************************************
+	 * Static Vars
+	 * *********************************************************************************************/
+	protected static HashMap<String, CompositeSurface> surfaces;
+	
+
+	/* *********************************************************************************************
+	 * Static Constructor
+	 * *********************************************************************************************/
+	static
+	{
+		surfaces = new HashMap<String, CompositeSurface>();
+		
+		CompositeSurface cube = new Cube();
+		surfaces.put("cube", cube);
+		surfaces.put("Cube", cube);
+		surfaces.put("CUBE", cube);
+		surfaces.put("box", cube);
+		surfaces.put("Box", cube);
+		surfaces.put("BOX", cube);
+	}
+	
+
+	/* *********************************************************************************************
+	 * Static Access Methods
+	 * *********************************************************************************************/
+	public static Instance create(String keyOrFilePath)
+	{
+		Instance inst = getInstance(keyOrFilePath);
+		
+		if(inst != null)
+			return inst;
+		
+		//If we can't create an instance, try to load the key/filePath
+		load(keyOrFilePath);
+		
+		//And now try again
+		inst = getInstance(keyOrFilePath);
+		
+		if(inst != null)
+			return inst;
+
+		//If we still failed to create an instance, return null
+		return null;
+	}
+	
+
+	/* *********************************************************************************************
+	 * Private Static Helper Methods
+	 * *********************************************************************************************/
+	private static Instance getInstance(String keyOrFilePath)
+	{
+		CompositeSurface cs = surfaces.get(keyOrFilePath);
+		
+		//Create a new wrapping instance for the current surface
+		if(cs != null)
+		{
+			Instance inst = new Instance();
+			inst.addChild(cs);
+			return inst;
+		}
+		
+		return null;
+	}
+	
+	private static void load(String keyOrFilePath)
+	{
+		//If the key/filePath has an .obj extension, then parse it as an object file
+		if(keyOrFilePath.endsWith(".obj"))
+		{
+			//TODO: Load object file and create a Mesh
+			ObjModelData data = ObjFileLoader.load(keyOrFilePath);
+			
+			if(data == null)
+				return;
+			
+			CompositeSurface model = new ObjectMeshSurface(data);
+			
+			surfaces.put(keyOrFilePath, model);
+			
+			return;
+		}
+		
+		//TODO: Add more loaders
+	}
+	
+	
+	/* *********************************************************************************************
+	 * Instance Vars
+	 * *********************************************************************************************/
+
+}
