d5a5f5c59fbf4b81bdda6085a3408b9eef895a14
Rex West <rexwest@gmail.com>
2014-04-17 14:59:29 -0700

    Implemented population of an ObjectSurface given a ObjModelData.Object

--- src/raytrace/geometry/meshes/ObjectSurface.java
+++ src/raytrace/geometry/meshes/ObjectSurface.java
@@ -1,8 +1,12 @@
 package raytrace.geometry.meshes;
 
+import process.logging.Logger;
 import raytrace.data.BakeData;
 import raytrace.data.IntersectionData;
 import raytrace.data.RayData;
+import raytrace.geometry.Triangle;
+import raytrace.geometry.Vertex;
+import raytrace.material.Material;
 import raytrace.surfaces.acceleration.AABVHSurface;
 import file.obj.ObjModelData;
 
@@ -40,15 +44,30 @@ public class ObjectSurface extends MeshSurface {
 		 * Create aabvh from triangles
 		 * set aabvh var
 		 */
+		Logger.progress(-14, "Starting creating an Object Surface....");
+		Logger.progress(-14, "Vertices: [" + obj.getVertices().size() + "].");
+		Logger.progress(-14, "Normals: [" + obj.getNormals().size() + "].");
+		Logger.progress(-14, "TexCoords: [" + obj.getTexCoords().size() + "].");
+		Logger.progress(-14, "Faces: [" + obj.getFaces().size() + "].");
 		
+		int[][] verts;
+		Triangle triangle;
 		for(ObjModelData.Face face : obj.getFaces())
 		{
+			verts = face.getVertices();
 			
+			triangle = new Triangle(new Vertex(obj.getVertex(verts[0][0]), obj.getNormal(verts[0][2]), obj.getTexCoord(verts[0][1])),
+									new Vertex(obj.getVertex(verts[1][0]), obj.getNormal(verts[1][2]), obj.getTexCoord(verts[1][1])),
+									new Vertex(obj.getVertex(verts[2][0]), obj.getNormal(verts[2][2]), obj.getTexCoord(verts[2][1])));
+
+			triangles.add(triangle);
 		}
 		
-		
-		
+		//Create and profile aabvh
+		long startTime = System.currentTimeMillis();
+		Logger.progress(-1, "Starting creating a BVH for root surface... [" + triangles.size() + " subsurfaces].");
 		aabvh = AABVHSurface.makeAABVH(triangles);
+		Logger.progress(-1, "Ending AABVH creation... (" + (System.currentTimeMillis() - startTime) + "ms).");
 	}
 
 	
@@ -71,6 +90,13 @@ public class ObjectSurface extends MeshSurface {
 	public void updateBoundingBox()
 	{
 		aabvh.updateBoundingBox();
+	}
+
+	@Override
+	public void setMaterial(Material material)
+	{
+		for(Triangle tri : triangles)
+			tri.setMaterial(material);
 	}
 
 }
