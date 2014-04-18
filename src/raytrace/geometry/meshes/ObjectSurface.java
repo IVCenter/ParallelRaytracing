7a4ca5e8146d7df285b45228b0f5409d4d0fe6e8
Rex West <rexwest@gmail.com>
2014-04-18 09:53:29 -0700

    Modified the parameterization of AABVH creation for subsurfaces of ObjectSurface

--- src/raytrace/geometry/meshes/ObjectSurface.java
+++ src/raytrace/geometry/meshes/ObjectSurface.java
@@ -50,23 +50,34 @@ public class ObjectSurface extends MeshSurface {
 		Logger.progress(-14, "TexCoords: [" + obj.getTexCoords().size() + "].");
 		Logger.progress(-14, "Faces: [" + obj.getFaces().size() + "].");
 		
+		//int tempCount = 0;
+		
 		int[][] verts;
 		Triangle triangle;
 		for(ObjModelData.Face face : obj.getFaces())
 		{
 			verts = face.getVertices();
 			
+			//TODO: If normals are at index 0, create from face
+			
 			triangle = new Triangle(new Vertex(obj.getVertex(verts[0][0]), obj.getNormal(verts[0][2]), obj.getTexCoord(verts[0][1])),
 									new Vertex(obj.getVertex(verts[1][0]), obj.getNormal(verts[1][2]), obj.getTexCoord(verts[1][1])),
 									new Vertex(obj.getVertex(verts[2][0]), obj.getNormal(verts[2][2]), obj.getTexCoord(verts[2][1])));
 
+			//if(triangle.getArea() <= 0.0 || triangle.getArea() > 1.0)
+			//	Logger.warning(-14, "ObjectSurface: Encountered Triangle with an area of [" + triangle.getArea() + "].");
+			
 			triangles.add(triangle);
+			
+			//tempCount++;
+			//if(tempCount > obj.getFaces().size() - 10)
+			//	triangle.print();
 		}
 		
 		//Create and profile aabvh
 		long startTime = System.currentTimeMillis();
 		Logger.progress(-1, "Starting creating a BVH for root surface... [" + triangles.size() + " subsurfaces].");
-		aabvh = AABVHSurface.makeAABVH(triangles);
+		aabvh = AABVHSurface.makeAABVH(triangles, 1, 4);
 		Logger.progress(-1, "Ending AABVH creation... (" + (System.currentTimeMillis() - startTime) + "ms).");
 	}
 
@@ -77,6 +88,11 @@ public class ObjectSurface extends MeshSurface {
 	@Override
 	public IntersectionData intersects(RayData data)
 	{
+		//IntersectionData idata = aabvh.intersects(data);
+		//if(idata != null) {
+		//	idata.setMaterial(this.material);
+		//}
+		//return idata;
 		return aabvh.intersects(data);
 	}
 	
@@ -92,11 +108,13 @@ public class ObjectSurface extends MeshSurface {
 		aabvh.updateBoundingBox();
 	}
 
+	/*
 	@Override
 	public void setMaterial(Material material)
 	{
 		for(Triangle tri : triangles)
 			tri.setMaterial(material);
 	}
+	*/
 
 }
