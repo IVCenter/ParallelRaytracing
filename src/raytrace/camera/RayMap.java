a19d190402299c11ef66a12e6da45459c6438a34
Rex West <rexwest@gmail.com>
2014-04-23 23:26:05 -0700

    Added a base class RayMap for programmably mapping pixels to rays

---
+++ src/raytrace/camera/RayMap.java
@@ -0,0 +1,12 @@
+package raytrace.camera;
+
+import math.Ray;
+import raytrace.map.Map2D;
+
+public abstract class RayMap extends Map2D<Ray> {
+	
+	/*
+	 * Typing for a 3D function that taxes x,y coords and returns a Ray
+	 */
+
+}
