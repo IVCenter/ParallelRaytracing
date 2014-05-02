0db1b9c3974521e69357a499ddd39f0cbf719d8e
Rex West <rexwest@gmail.com>
2014-05-02 13:29:48 -0700

    Added the ability for the NodeManager to manage Nodes (including adding, removal, iterating, hashing by id, etc.)

--- src/network/NodeManager.java
+++ src/network/NodeManager.java
@@ -1,13 +1,77 @@
 package network;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 
-public class NodeManager implements Iterable<Node> {
+import process.logging.Logger;
 
+public class NodeManager implements Iterable<Node> {
+	
+	/*
+	 * A class for managing nodes
+	 */
+
+	/* *********************************************************************************************
+	 * Instance Vars
+	 * *********************************************************************************************/
+	protected ArrayList<Node> nodes;
+	protected HashMap<String, Node> idToNodeMap;
+	
+
+	/* *********************************************************************************************
+	 * Cosntructor
+	 * *********************************************************************************************/
+	public NodeManager()
+	{
+		nodes = new ArrayList<Node>();
+		idToNodeMap = new HashMap<String, Node>();
+	}
+
+	/* *********************************************************************************************
+	 * Mutation Methods
+	 * *********************************************************************************************/
+	public void addNode(Node node)
+	{
+		if(idToNodeMap.containsKey(node.getId()))
+		{
+			Logger.warning(-25, "NodeManager: Can not add two nodes with the same ID [" + node.getId() + "].");
+			return;
+		}
+		
+		idToNodeMap.put(node.getId(), node);
+		nodes.add(node);
+	}
+	
+	public void removeNode(Node node)
+	{
+		if(idToNodeMap.containsKey(node.getId()))
+		{
+			Logger.warning(-25, "NodeManager: Can not remove a node it does not contain. Node ID [" + node.getId() + "].");
+			return;
+		}
+		
+		idToNodeMap.put(node.getId(), null);
+		nodes.remove(node);
+	}
+
+	
+	/* *********************************************************************************************
+	 * Non-Mutation Methods
+	 * *********************************************************************************************/
+	public int getNodeCount()
+	{
+		return nodes.size();
+	}
+	
+	
+	/* *********************************************************************************************
+	 * Iterable Overrides
+	 * *********************************************************************************************/
 	@Override
-	public Iterator<Node> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+	public Iterator<Node> iterator()
+	{
+		return nodes.iterator();
 	}
 
 }
