ed30e4a01914e956ba109b5a899a308cfadcb2bd
Rex West <rexwest@gmail.com>
2014-05-05 00:10:48 -0700

    Modified Launcher in preparation for file based configuration. Nodes now default to headless compute nodes on start up unless otherwise configured.

--- src/system/Launcher.java
+++ src/system/Launcher.java
@@ -1,5 +1,7 @@
 package system;
 
+import java.io.File;
+
 import process.Environment;
 import process.logging.Logger;
 import raytrace.scene.SceneLoader;
@@ -15,36 +17,66 @@ public class Launcher {
 		/*
 		 * Args:
 		 * 
-		 * 		-
+		 * 		-[0] = Configuration File 
 		 */
 		
-		//TODO: Parse the arguments
-		//TODO: Set configuration values
-		//TODO: Move these to each scene, or use a config file
+		//Set up some defaults
 		Configuration.setId(Constants.Default.NODE_ID);
-		//Configuration.setScreenWidth(1368);
-		//Configuration.setScreenHeight(752);
-		Configuration.setScreenWidth(800);
-		Configuration.setScreenHeight(600);
-		Configuration.setDrawToScreen(true);
-		Configuration.setClock(true);
-		Configuration.setLeaf(true);
-		Configuration.setController(true);
-		
-		//TODO: Need a reliable and relative folder to write to
-		Configuration.setWorkingDirectory("/Users/Asylodus/Desktop/NightSky/");
+		Configuration.setDrawToScreen(false);
+		Configuration.setClock(false);
+		Configuration.setLeaf(false);
+		Configuration.setController(false);
 		
 		
+		//Setup debug configuration
+		if(args.length > 0 && args[0].equals("DEBUG"))
+		{
+			Configuration.setId("Debug Node");
+			//Configuration.setScreenWidth(1368);
+			//Configuration.setScreenHeight(752);
+			Configuration.setScreenWidth(800);
+			Configuration.setScreenHeight(600);
+			Configuration.setDrawToScreen(true);
+			Configuration.setClock(true);
+			Configuration.setLeaf(true);
+			Configuration.setController(true);
+			//TODO: Need a reliable and relative folder to write to
+			Configuration.setWorkingDirectory("/Users/Asylodus/Desktop/NightSky/");
+			Configuration.setMasterScene(SceneLoader.load(Constants.SceneKeys.CSE168_Project2));
+		}
 		
-		//Load the scene (TODO: Parse args for scene choice)
-		String sceneKey = Constants.SceneKeys.CSE168_Project2;
-		Configuration.setMasterScene(SceneLoader.load(sceneKey));
 		
-		Logger.progress(-1, "Launching a Parallel Rendering Node with ID:[" + Configuration.getId() + "]...");
+		//Try to open the config file
+		File configFile = null;
+		try{
+			
+			configFile = new File(args[0]);
+			
+			//If we have a config file, 
+			if(configFile.exists() && configFile.getName().endsWith(Constants.configurationFileExtension))
+			{
+				//TODO: Config file loader
+			}
+			
+		}catch(Exception e) {
+			//Do nothing, this is acceptable
+		}
+		
+		
+		Logger.progress(-1, "Launching a Night Sky Node with ID:[" + Configuration.getId() + "]...");
 		
 		//Pass off control to the ApplicationDelegate
 		ApplicationDelegate app = new ApplicationDelegate();
 		app.execute(new Environment());
 	}
+	
+	/*
+	 * Local TOOD:
+	 * 
+	 * 		-Network Renderer
+	 * 		-Render/Update Request and Responses
+	 * 		-Intermediates
+	 * 		-
+	 */
 
 }
