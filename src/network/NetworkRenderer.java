1a124156aa685203010bf53ff66dce859bd4984b
Rex West <rexwest@gmail.com>
2014-05-05 00:12:02 -0700

    Added a network renderer that distributes rendering to compute nodes.

--- src/network/NetworkRenderer.java
+++ src/network/NetworkRenderer.java
@@ -1,6 +1,10 @@
 package network;
 
+import java.util.Iterator;
+
+import network.send.MessageSender;
 import process.logging.Logger;
+import raytrace.camera.Camera;
 import raytrace.data.RenderData;
 import raytrace.data.UpdateData;
 import raytrace.framework.Renderer;
@@ -14,14 +18,18 @@ public class NetworkRenderer implements Renderer {
 	 * Instance Vars
 	 * *********************************************************************************************/
 	protected NodeManager nodeManager;
+	protected MessageSender messageSender;
+	
+	protected int outstandingRequests = 0;
 	
 	
 	/* *********************************************************************************************
 	 * Constructors
 	 * *********************************************************************************************/
-	public NetworkRenderer(NodeManager nodeManager)
+	public NetworkRenderer(NodeManager nodeManager, MessageSender messageSender)
 	{
 		this.nodeManager = nodeManager;
+		this.messageSender = messageSender;
 	}
 	
 
@@ -44,13 +52,57 @@ public class NetworkRenderer implements Renderer {
 		 * Loop nodes
 		 * 		send update message
 		 */
+		
+		//For all nodes, send an update request
+		for(Node node : nodeManager)
+		{
+			Message message = CommonMessageConstructor.createUpdateRequestMessage(data);
+			messageSender.send(message, node.ip);
+			
+			outstandingRequests++;
+		}
+		
+		//Wait until all nodes are done
+		waitOnOutstandingRequests();
 	}
 
 	@Override
 	public void render(RenderData data)
 	{
 		Logger.progress(-1, "Rendering...");
+		
+		//TODO: Per-frame Load balance
+		
+		//TODO: Decompose Camera
+		Iterator<Camera> cameras = data.getScene().getActiveCamera().decompose(nodeManager.getNodeCount()).iterator();
+		
 		//
+		
+		//For all nodes, send a render request
+		for(Node node : nodeManager)
+		{
+			Message message = CommonMessageConstructor.createRenderRequestMessage(cameras.next());
+			messageSender.send(message, node.ip);
+			
+			outstandingRequests++;
+		}
+		
+		//Wait until all nodes are done
+		waitOnOutstandingRequests();
+	}
+	
+	private void waitOnOutstandingRequests()
+	{
+		//Wait until all requests are completed
+		while(outstandingRequests > 0) {
+			synchronized (this) {
+				try {
+					this.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
 	
@@ -58,5 +110,22 @@ public class NetworkRenderer implements Renderer {
 	 * Getter/Setter Methods
 	 * *********************************************************************************************/
 	//
+	public int getOutstandingRequests() {
+		return outstandingRequests;
+	}
+	
+	//Used by response handlers to alert the renderer when each node completes updating/rendering
+	public synchronized void completedARequest()
+	{
+		if(outstandingRequests <= 0)
+			return;
+
+		outstandingRequests -= 1;
+		
+		if(outstandingRequests == 0)
+		{
+			this.notifyAll();
+		}
+	}
 
 }
