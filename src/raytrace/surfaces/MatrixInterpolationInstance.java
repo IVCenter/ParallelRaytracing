98e7f214fd70ebeabf0d196c77e5473e415e8fb8
Rex West <rexwest@gmail.com>
2014-05-09 10:00:22 -0700

    Added a subclass of Instance for interpolating an instance between a start and end matrix: MatrixInterpolationInstance.

---
+++ src/raytrace/surfaces/MatrixInterpolationInstance.java
@@ -0,0 +1,82 @@
+package raytrace.surfaces;
+
+import math.Matrix4;
+import raytrace.data.IntersectionData;
+import raytrace.data.RayData;
+import raytrace.material.Material;
+
+public class MatrixInterpolationInstance extends Instance {
+	
+	/*
+	 * A convenient extension ;)
+	 */
+	/* *********************************************************************************************
+	 * Instance Vars
+	 * *********************************************************************************************/
+	protected Matrix4 startMatrix;
+	protected Matrix4 endMatrix;
+	
+
+	/* *********************************************************************************************
+	 * Constructor
+	 * *********************************************************************************************/
+	public MatrixInterpolationInstance()
+	{
+		//
+		super();
+	}
+	
+	public MatrixInterpolationInstance(Matrix4 startMatrix, Matrix4 endMatrix)
+	{
+		super();
+		this.startMatrix = startMatrix;
+		this.endMatrix = endMatrix;
+	}
+	
+
+	/* *********************************************************************************************
+	 * Overrides
+	 * *********************************************************************************************/
+	@Override
+	public void setMaterial(Material material)
+	{
+		this.material = material;
+	}
+	
+	@Override
+	public IntersectionData intersects(RayData data)
+	{
+		//Get time
+		//Interpolate matrix
+		//Set matrix
+		
+		//As awkward as this seems, it works.
+		double time = data.getRay().getRandomValue();
+		
+		
+		
+		
+		
+		return super.intersects(data);
+	}
+	
+
+	/* *********************************************************************************************
+	 * Geters/Setters
+	 * *********************************************************************************************/
+	public Matrix4 getStartMatrix() {
+		return startMatrix;
+	}
+
+	public void setStartMatrix(Matrix4 startMatrix) {
+		this.startMatrix = startMatrix;
+	}
+
+	public Matrix4 getEndMatrix() {
+		return endMatrix;
+	}
+
+	public void setEndMatrix(Matrix4 endMatrix) {
+		this.endMatrix = endMatrix;
+	}
+}
