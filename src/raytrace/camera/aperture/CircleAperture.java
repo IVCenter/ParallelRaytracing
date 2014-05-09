10f0bb5a249ab7eac2895550ac6b21daecb7d57a
Rex West <rexwest@gmail.com>
2014-05-08 20:38:25 -0700

    Implemented a simple circular aperture.

---
+++ src/raytrace/camera/aperture/CircleAperture.java
@@ -0,0 +1,62 @@
+package raytrace.camera.aperture;
+
+import math.Vector4;
+
+public class CircleAperture implements Aperture {
+	
+	private static final long serialVersionUID = 1L;
+	
+	/*
+	 * A simple circle aperture
+	 */
+	/* *********************************************************************************************
+	 * Instance Vars
+	 * *********************************************************************************************/
+	protected double radius;
+	
+
+	/* *********************************************************************************************
+	 * Constructor
+	 * *********************************************************************************************/
+	public CircleAperture()
+	{
+		radius = 0.0;
+	}
+	
+	public CircleAperture(double radius)
+	{
+		super();
+		this.radius = radius;
+	}
+
+	/* *********************************************************************************************
+	 * Interface Overrides
+	 * *********************************************************************************************/
+	@Override
+	public Vector4 sample()
+	{
+		return sample(new Vector4());
+	}
+	
+	@Override
+	public Vector4 sample(Vector4 sample)
+	{
+		double theta = Math.random() * Math.PI * 2.0;
+		double distance = Math.random() * radius;
+		sample.set(distance * Math.cos(theta), distance * Math.sin(theta), 0, 0);
+		return sample;
+	}
+	
+	
+	/* *********************************************************************************************
+	 * Getters/Setters
+	 * *********************************************************************************************/
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+}
