d44179ae7b63678bea352aadfd9e3c2af40eaff2
Rex West <rexwest@gmail.com>
2014-05-22 13:36:23 -0700

    Added a utility method to perform single splits to StringParser.

--- src/file/StringParser.java
+++ src/file/StringParser.java
@@ -2,6 +2,7 @@ package file;
 
 import java.util.Map;
 
+
 public abstract class StringParser<POPULATES_THIS_TYPE> {
 	
 	/*
@@ -24,6 +25,29 @@ public abstract class StringParser<POPULATES_THIS_TYPE> {
 	protected String[] tokens(String str, String pattern)
 	{
 		return str.trim().split(pattern);
+	}
+	
+	protected String[] splitAtFirst(String str, String character)
+	{
+		int firstIndex = str.indexOf(character);
+		
+		//Skip empty strings or strings that don't have the splitting token
+		if(str.length() == 0 || firstIndex < 0) {
+			String[] result = {str};
+			return result;
+		}
+		
+		//Get the line key
+		String[] result = new String[2];
+		result[0] = str.substring(0, firstIndex);
+		
+		if(firstIndex < str.length()-1) {
+			result[1] =  str.substring(firstIndex+1, str.length());
+		}else{
+			result[1] = "";
+		}
+		
+		return result;
 	}
 	
 	public String getKeyToken() { return keyToken; }
