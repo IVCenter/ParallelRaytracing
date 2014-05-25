0e70e675c9d81fdfd929fc79f70d34ac583619c4
Rex West <rexwest@gmail.com>
2014-05-25 14:20:19 -0700

    Updated launcher to match the new structure for configuration files and automatic directory generation.

--- src/system/Launcher.java
+++ src/system/Launcher.java
@@ -3,6 +3,7 @@ package system;
 import java.io.File;
 
 import file.nsconfig.ConfigFileLoader;
+import folder.DirectoryManager;
 
 import process.Environment;
 import process.logging.Logger;
@@ -22,37 +23,6 @@ public class Launcher {
 		 * 		-[0] = Configuration File 
 		 */
 		
-		
-		
-		
-		//Set up some defaults
-		Configuration.setId(Constants.Default.NODE_ID);
-		Configuration.setDrawToScreen(false);
-		Configuration.setClock(false);
-		Configuration.setLeaf(false);
-		Configuration.setController(false);
-		//Configuration.setWorkingDirectory("/home/linux/ieng6/oce/2d/rwest/NightSky/");
-		
-		
-		//Setup debug configuration
-		if(args.length > 0 && args[0].equals("DEBUG"))
-		{
-			Configuration.setId("Debug Node");
-			Configuration.setScreenWidth(1368);
-			Configuration.setScreenHeight(752);
-			//Configuration.setScreenWidth(800);
-			//Configuration.setScreenHeight(600);
-			Configuration.setDrawToScreen(true);
-			Configuration.setClock(true);
-			Configuration.setLeaf(true);//true for local, false for networked
-			Configuration.setController(true);
-			//TODO: Need a reliable and relative folder to write to
-			Configuration.setWorkingDirectory("/Users/Asylodus/Desktop/NightSky/");
-			//TODO: If working directory doesnt have a slash at the end, add it
-			Configuration.setMasterScene(SceneLoader.load(Constants.SceneKeys.TEST5));
-		}
-		
-		
 		//Try to open the config file
 		File configFile = null;
 		try{
@@ -63,20 +33,23 @@ public class Launcher {
 			if(configFile.exists() && configFile.getName().endsWith(Constants.configurationFileExtension))
 			{
 				ConfigFileLoader.load(configFile);
-				
-				//TODO: Create folder structure if necessary
-				//See if working dir exists, if not see if parent exists, if not set writing flag to false
-				//if necessary create working dir under exisitng parent
-				//if necessary create sub folders
+
+				//Create the directory structure for the current config
+				DirectoryManager.createFolderStructure();
+			}else{
+				Logger.error(-1, "Launcher: Unable to open the configuration file [" + args[0] + "].");
 			}
 			
 		}catch(Exception e) {
 			//Do nothing, this is acceptable
 		}
 		
-
-		//TODO: If working dir from args is missing a trailing slash, add it
 		
+		//Setup debug configuration
+		if(args.length > 0 && args[0].equals("DEBUG"))
+		{
+			loadDebugConfiguration();
+		}
 		
 		
 		Logger.progress(-1, "Launching a Night Sky Node with ID:[" + Configuration.getId() + "]...");
@@ -84,6 +57,25 @@ public class Launcher {
 		//Pass off control to the ApplicationDelegate
 		ApplicationDelegate app = new ApplicationDelegate();
 		app.execute(new Environment());
+	}
+	
+	private static void loadDebugConfiguration()
+	{
+		//Feel free to over write these with your own settings
+		Configuration.setId("Debug Node");
+		Configuration.setScreenWidth(1368);
+		Configuration.setScreenHeight(752);
+		//Configuration.setScreenWidth(800);
+		//Configuration.setScreenHeight(600);
+		Configuration.setDrawToScreen(true);
+		Configuration.setClock(true);//The top most node must have clock set to true (this includes stand-alone nodes)
+		Configuration.setLeaf(true);//true for local, false for networked
+		Configuration.setController(true);
+		Configuration.setWorkingDirectory("/Users/Asylodus/Desktop/NightSky/");
+		Configuration.setMasterScene(SceneLoader.load(Constants.SceneKeys.TEST5));
+
+		//Create the directory structure for the debug config
+		DirectoryManager.createFolderStructure();
 	}
 	
 	/*
@@ -99,6 +91,7 @@ public class Launcher {
 		//TODO: Add Quad support to Triangle
 		//TODO: Discrete texture class that loads bmps, pngs, etc.
 		//TODO: A transform wrapper for texture
+		//TODO: Shutdown request
 	 */
 
 }
