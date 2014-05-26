e25fc79dd3429078fb8de04e934bfa666bffd2b7
Rex West <rexwest@gmail.com>
2014-05-26 12:41:14 -0700

    Added a Pinhole Aperture (it operates identically to a Circular Aperture of radius zero, but it less computationally expensive, and better named.)

---
+++ src/raytrace/camera/aperture/PinholeAperture.java
@@ -0,0 +1,36 @@
+package raytrace.camera.aperture;
+
+import math.Vector4;
+
+public class PinholeAperture implements Aperture {
+	
+	private static final long serialVersionUID = 1L;
+	
+	/*
+	 * A simple pinhole aperture
+	 */
+	/* *********************************************************************************************
+	 * Constructor
+	 * *********************************************************************************************/
+	public PinholeAperture()
+	{	
+		/**/
+	}
+
+	/* *********************************************************************************************
+	 * Interface Overrides
+	 * *********************************************************************************************/
+	@Override
+	public Vector4 sample()
+	{
+		return sample(new Vector4());
+	}
+	
+	@Override
+	public Vector4 sample(Vector4 sample)
+	{
+		sample.set(0, 0, 0, 0);
+		return sample;
+	}
+
+}
