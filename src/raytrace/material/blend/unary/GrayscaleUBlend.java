42eeefd5385b471491fdbe963e5e699a47c1fc14
Rex West <rexwest@gmail.com>
2014-05-27 18:52:14 -0700

    Added a unary blend that converts the material color to greyscale.

---
+++ src/raytrace/material/blend/unary/GrayscaleUBlend.java
@@ -0,0 +1,32 @@
+package raytrace.material.blend.unary;
+
+import raytrace.color.Color;
+import raytrace.material.Material;
+
+public class GrayscaleUBlend extends UnaryBlend {
+
+	/*
+	 * A unary blend converting a material shade to grayscale.
+	 */
+	
+	/* *********************************************************************************************
+	 * Constructor
+	 * *********************************************************************************************/
+	public GrayscaleUBlend(Material material)
+	{
+		super(material);
+	}
+	
+	
+	/* *********************************************************************************************
+	 * Blend Override
+	 * *********************************************************************************************/
+	@Override
+	public Color blend(Color shade)
+	{
+		double[] channels = shade.getChannels();
+		double avg = (channels[0] + channels[1] + channels[2]) / 3.0;
+		return new Color(avg, avg, avg, channels[3]);
+	}
+
+}
