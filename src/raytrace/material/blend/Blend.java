a51569c7688fef29854128706721a88f38c8c050
Rex West <rexwest@gmail.com>
2014-05-27 18:51:13 -0700

    Added an interface for Blends.

---
+++ src/raytrace/material/blend/Blend.java
@@ -0,0 +1,12 @@
+package raytrace.material.blend;
+
+import raytrace.material.Material;
+
+
+public interface Blend extends Iterable<Material>{
+	
+	/*
+	 * An interface for all Blend types
+	 */
+
+}
