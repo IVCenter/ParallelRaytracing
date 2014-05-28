d83a609c627e7aae71b5e09a67bed8e5976e44c9
Rex West <rexwest@gmail.com>
2014-05-27 18:51:46 -0700

    Added a base class for Ternary Blends.

---
+++ src/raytrace/material/blend/ternary/TernaryBlend.java
@@ -0,0 +1,126 @@
+package raytrace.material.blend.ternary;
+
+import java.util.Iterator;
+
+import raytrace.color.Color;
+import raytrace.data.ShadingData;
+import raytrace.material.Material;
+import raytrace.material.blend.Blend;
+
+public abstract class TernaryBlend extends Material implements Blend {
+	
+	/*
+	 * A base class for binary blends
+	 */
+	
+	/* *********************************************************************************************
+	 * Instance Vars
+	 * *********************************************************************************************/
+	protected Material firstMaterial;
+	protected Material secondMaterial;
+	protected Material thirdMaterial;
+	
+
+	/* *********************************************************************************************
+	 * Cosntructors
+	 * *********************************************************************************************/
+	public TernaryBlend(Material firstMaterial, Material secondMaterial, Material thirdMaterial)
+	{
+		this.firstMaterial = firstMaterial;
+		this.secondMaterial = secondMaterial;
+		this.thirdMaterial = thirdMaterial;
+	}
+	
+	
+	/* *********************************************************************************************
+	 * Abstract Blend Method
+	 * *********************************************************************************************/
+	public abstract Color blend(Color firstShade, Color secondShade, Color thirdShade);
+
+
+	/* *********************************************************************************************
+	 * Material Overrides
+	 * *********************************************************************************************/
+	@Override
+	public Color shade(ShadingData data)
+	{
+		return blend(firstMaterial.shade(data), secondMaterial.shade(data), thirdMaterial.shade(data));
+	}
+
+
+	/* *********************************************************************************************
+	 * Setters/Getters
+	 * *********************************************************************************************/	
+	public Material getFirstMaterial() {
+		return firstMaterial;
+	}
+
+	public void setFirstMaterial(Material firstMaterial) {
+		this.firstMaterial = firstMaterial;
+	}
+
+	public Material getSecondMaterial() {
+		return secondMaterial;
+	}
+
+	public void setSecondMaterial(Material secondMaterial) {
+		this.secondMaterial = secondMaterial;
+	}
+	
+	public Material getThirdMaterial() {
+		return thirdMaterial;
+	}
+
+	public void setThirdMaterial(Material thirdMaterial) {
+		this.thirdMaterial = thirdMaterial;
+	}
+
+
+	/* *********************************************************************************************
+	 * Iterator Overtide
+	 * *********************************************************************************************/
+	@Override
+	public Iterator<Material> iterator()
+	{
+		return new MaterialIterator();
+	}
+
+	/* *********************************************************************************************
+	 * Private Classes
+	 * *********************************************************************************************/
+	private class MaterialIterator implements Iterator<Material>
+	{	
+		int materialIndex = 0;
+		
+		public MaterialIterator() { /**/ }
+		
+		@Override
+		public boolean hasNext()
+		{
+			return materialIndex < 3;
+		}
+
+		@Override
+		public Material next()
+		{
+			if(materialIndex == 0) {
+				return firstMaterial;
+			}else if(materialIndex == 1){
+				return secondMaterial;
+			}else if(materialIndex == 2){
+				return thirdMaterial;
+			}
+			
+			materialIndex++;
+			
+			return null;
+		}
+
+		@Override
+		public void remove()
+		{
+			//No
+		}
+		
+	}
+}
