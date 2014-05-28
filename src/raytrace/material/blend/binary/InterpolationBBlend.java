138e2daabb0c92020581a58dee7a0009def0ed0e
Rex West <rexwest@gmail.com>
2014-05-27 18:52:46 -0700

    Added a binary blend that interpolates between two materials.

---
+++ src/raytrace/material/blend/binary/InterpolationBBlend.java
@@ -0,0 +1,48 @@
+package raytrace.material.blend.binary;
+
+import raytrace.color.Color;
+import raytrace.material.Material;
+
+public class InterpolationBBlend  extends BinaryBlend {
+
+	/*
+	 * A binary blend for interpolating between two materials
+	 */
+	
+	/* *********************************************************************************************
+	 * Instance Vars
+	 * *********************************************************************************************/
+	protected double t;
+	
+	
+	/* *********************************************************************************************
+	 * Constructor
+	 * *********************************************************************************************/
+	public InterpolationBBlend(Material firstMaterial, Material secondMaterial, double t)
+	{
+		super(firstMaterial, secondMaterial);
+		this.t = t;
+	}
+	
+	
+	/* *********************************************************************************************
+	 * Blend Override
+	 * *********************************************************************************************/
+	@Override
+	public Color blend(Color firstShade, Color secondShade)
+	{
+		return firstShade.interpolate(firstShade, secondShade, t);
+	}
+	
+	
+	/* *********************************************************************************************
+	 * Getters/Setters
+	 * *********************************************************************************************/
+	public double getT() {
+		return t;
+	}
+
+	public void setT(double t) {
+		this.t = t;
+	}
+}
