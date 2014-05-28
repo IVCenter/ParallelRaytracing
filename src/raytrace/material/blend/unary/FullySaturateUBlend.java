ca7408876a6cd712280119c0f8d4ec7498cadb73
Rex West <rexwest@gmail.com>
2014-05-27 18:52:31 -0700

    Added a unary blend that converts a material color to full saturated.

---
+++ src/raytrace/material/blend/unary/FullySaturateUBlend.java
@@ -0,0 +1,41 @@
+package raytrace.material.blend.unary;
+
+import raytrace.color.Color;
+import raytrace.material.Material;
+
+public class FullySaturateUBlend extends UnaryBlend {
+
+	/*
+	 * A unary blend converting a material shade to full saturated.
+	 */
+	
+	/* *********************************************************************************************
+	 * Constructor
+	 * *********************************************************************************************/
+	public FullySaturateUBlend(Material material)
+	{
+		super(material);
+	}
+	
+	
+	/* *********************************************************************************************
+	 * Blend Override
+	 * *********************************************************************************************/
+	@Override
+	public Color blend(Color shade)
+	{
+		double[] channels = shade.getChannels();
+		double min = Math.min(Math.min(channels[0], channels[1]), channels[2]);
+		double max = Math.max(Math.max(channels[0], channels[1]), channels[2]);
+		double scale = 1.0 / (max - min);
+		
+		Color saturatedColor = new Color((channels[0] - min) * scale, 
+										 (channels[1] - min) * scale, 
+										 (channels[2] - min) * scale, 
+										 channels[3]);
+		
+		saturatedColor.multiply3M(max);
+		return saturatedColor;
+	}
+
+}
