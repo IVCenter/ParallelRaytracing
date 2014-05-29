dcf697ac4b8c69d6004f42ec3fabdb6bcb9b8642
Rex West <rexwest@gmail.com>
2014-05-29 02:45:35 -0700

    Added a binary blend for adding a poster effect to a mask, PosterMaskBBlend.

---
+++ src/raytrace/material/blend/binary/PosterMaskBBlend.java
@@ -0,0 +1,72 @@
+package raytrace.material.blend.binary;
+
+import raytrace.color.Color;
+import raytrace.data.ShadingData;
+import raytrace.map.texture.Texture;
+import raytrace.material.Material;
+
+public class PosterMaskBBlend extends BinaryBlend {
+
+	/*
+	 * A binary blend for interpolating between two materials using a texture mask
+	 */
+	
+	/* *********************************************************************************************
+	 * Instance Vars
+	 * *********************************************************************************************/
+	protected Texture mask;
+	protected double level;
+	
+	
+	/* *********************************************************************************************
+	 * Constructor
+	 * *********************************************************************************************/
+	public PosterMaskBBlend(Material firstMaterial, Material secondMaterial)
+	{
+		super(firstMaterial, secondMaterial);
+		this.mask = Color.gray();
+		this.level = 0.5;
+	}
+	
+	public PosterMaskBBlend(Material firstMaterial, Material secondMaterial, Texture mask, double level)
+	{
+		super(firstMaterial, secondMaterial);
+		this.mask = mask;
+		this.level = level;
+	}
+	
+	
+	/* *********************************************************************************************
+	 * Blend Override
+	 * *********************************************************************************************/
+	@Override
+	public Color blend(Color firstShade, Color secondShade, ShadingData data)
+	{
+		double[] firstColor = firstShade.getChannels();
+		double[] secondColor = secondShade.getChannels();
+		double[] maskColor = mask.evaluate(data.getIntersectionData()).duplicate().clamp3M().getChannels();
+		return new Color(maskColor[0] < level ? firstColor[0] : secondColor[0],
+				 		 maskColor[1] < level ? firstColor[1] : secondColor[1],
+				 		 maskColor[2] < level ? firstColor[2] : secondColor[2]);
+	}
+	
+	
+	/* *********************************************************************************************
+	 * Getters/Setters
+	 * *********************************************************************************************/
+	public Texture getMask() {
+		return mask;
+	}
+
+	public void setMask(Texture mask) {
+		this.mask = mask;
+	}
+
+	public double getLevel() {
+		return level;
+	}
+
+	public void setLevel(double level) {
+		this.level = level;
+	}
+}
