61e8b0a916bf4affaf0fc72af11adae3d8e4e3e2
Rex West <rexwest@gmail.com>
2014-05-31 19:53:18 -0700

    Added a circular gradient 3d texture.

---
+++ src/raytrace/map/texture/_3D/CircularGradientTexture3D.java
@@ -0,0 +1,83 @@
+package raytrace.map.texture._3D;
+
+import raytrace.color.Color;
+
+public class CircularGradientTexture3D extends Texture3D {
+	
+	/*
+	 * A gradient texture in two dimensions
+	 */
+	
+	/* *********************************************************************************************
+	 * Instance Vars
+	 * *********************************************************************************************/
+	protected double weight;
+	protected Color firstColor;
+	protected Color secondColor;
+	
+
+	/* *********************************************************************************************
+	 * Constructor
+	 * *********************************************************************************************/
+	public CircularGradientTexture3D()
+	{
+		this.firstColor = Color.black();
+		this.secondColor = Color.white();
+		this.weight = 1.0;
+	}
+	
+	public CircularGradientTexture3D(Color firstColor, Color secondColor)
+	{
+		this.firstColor = firstColor;
+		this.secondColor = secondColor;
+		this.weight = 1.0;
+	}
+	
+	public CircularGradientTexture3D(Color firstColor, Color secondColor, double weight)
+	{
+		this.firstColor = firstColor;
+		this.secondColor = secondColor;
+		this.weight = weight;
+	}
+	
+
+	/* *********************************************************************************************
+	 * Texture3D Override
+	 * *********************************************************************************************/
+	@Override
+	public Color evaluate(Double x, Double y, Double z)
+	{
+		double p = Math.sqrt(x * x + z * z);
+		double t = Math.min(1.0, Math.max(0.0, p));
+		return firstColor.interpolate(firstColor, secondColor, Math.pow(t, weight));
+	}
+	
+	
+	/* *********************************************************************************************
+	 * Getters/Setters
+	 * *********************************************************************************************/
+	public Color getFirstColor() {
+		return firstColor;
+	}
+
+	public void setFirstColor(Color firstColor) {
+		this.firstColor = firstColor;
+	}
+
+	public Color getSecondColor() {
+		return secondColor;
+	}
+
+	public void setSecondColor(Color secondColor) {
+		this.secondColor = secondColor;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+}
