5be8b82d7f3f21b0d61560a7cb408467c8ac6f15
Rex West <rexwest@gmail.com>
2014-06-02 04:07:14 -0700

    Added a vertex synchronization method to MeshSurface.

--- src/raytrace/geometry/meshes/MeshSurface.java
+++ src/raytrace/geometry/meshes/MeshSurface.java
@@ -10,6 +10,7 @@ import raytrace.data.BakeData;
 import raytrace.data.IntersectionData;
 import raytrace.data.RayData;
 import raytrace.geometry.Triangle;
+import raytrace.geometry.Vertex;
 import raytrace.surfaces.TerminalSurface;
 
 public class MeshSurface extends TerminalSurface {
@@ -95,6 +96,61 @@ public class MeshSurface extends TerminalSurface {
 			closest.setMeshID(meshID);
 		
 		return closest;
+	}
+	
+	public void synchronizeVertices()
+	{
+		//Finds are vertices at the same location and converts them to the same instance
+		//This is useful for procedural geometry where it is expensive to keep track of 
+		//	vertices on shared edges
+		
+		HashMap<String, Vector4> seenVectors = new HashMap<String, Vector4>();
+		
+		String encoding;
+		Vector4 existing;
+		for(Triangle tri : triangles)
+		{
+			for(Vertex vert : tri.getVertices())
+			{
+				encoding = encodeVector(vert.getPosition());
+				existing = seenVectors.get(encoding);
+				
+				if(existing == null) {
+					existing = vert.getPosition();
+					seenVectors.put(encoding, existing);
+				}
+				
+				vert.setPosition(existing);
+			}
+		}
+	}
+	
+	public void smoothNormals()
+	{
+		//Another encoding and hash based method for fast processing of triangle normals
+	}
+	
+	public void tessellateMeshByTriangleAreaConstraint(double areaConstraint)
+	{
+		//
+	}
+	
+	private String encodeVector(Vector4 v)
+	{
+		StringBuilder sb = new StringBuilder();
+		
+		//The float cast here is a terrible idea
+		sb.append("<");
+		sb.append(Double.toString((float)v.get(0)));
+		sb.append(", ");
+		sb.append(Double.toString((float)v.get(1)));
+		sb.append(", ");
+		sb.append(Double.toString((float)v.get(2)));
+		sb.append(", ");
+		sb.append(Double.toString((float)v.get(3)));
+		sb.append(">");
+		
+		return sb.toString();
 	}
 	
 	/**
