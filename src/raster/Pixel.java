eb433d3004d8d8b57ab8c1ab190c8113fbb32c8a
Rex West <rexwest@gmail.com>
2015-02-24 10:05:24 +0900

    Updated the RenderBuffer implementation

---
+++ src/raster/Pixel.java
@@ -0,0 +1,65 @@
+package raster;
+
+import math.Vector3;
+import raytrace.color.Color;
+
+public class Pixel {
+
+
+	/*
+	 * A storage structure for pixel data
+	 */
+
+	/* *********************************************************************************************
+	 * Instance Vars
+	 * *********************************************************************************************/
+	protected Color color;
+	protected Vector3 point;
+	protected Vector3 normal;
+	protected double depth;
+
+
+	/* *********************************************************************************************
+	 * Constructors
+	 * *********************************************************************************************/
+	public Pixel()
+	{
+		color = new Color();
+		point = new Vector3();
+		normal = new Vector3();
+		depth = Double.POSITIVE_INFINITY;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public Vector3 getPoint() {
+		return point;
+	}
+
+	public void setPoint(Vector3 point) {
+		this.point = point;
+	}
+
+	public Vector3 getNormal() {
+		return normal;
+	}
+
+	public void setNormal(Vector3 normal) {
+		this.normal = normal;
+	}
+
+	public double getDepth() {
+		return depth;
+	}
+
+	public void setDepth(double depth) {
+		this.depth = depth;
+	}
+	
+}
--- src/raster/RenderBuffer.java
+++ src/raster/RenderBuffer.java
@@ -3,11 +3,45 @@ package raster;
 public class RenderBuffer {
 	
 	/*
-	 * 
+	 * A buffer for storing render data for each pixel of the render frame
 	 */
 
 	/* *********************************************************************************************
 	 * Instance Vars
 	 * *********************************************************************************************/
+	protected int width;
+	protected int height;
+	protected Pixel[] pixels;
 
+	
+	/* *********************************************************************************************
+	 * Constructors
+	 * *********************************************************************************************/
+	public RenderBuffer(int width, int height)
+	{
+		this.width = width;
+		this.height = height;
+		
+		pixels = new Pixel[width * height];
+		
+		for(int i = 0; i < pixels.length; ++i)
+			pixels[i] = new Pixel();
+	}
+	
+	
+	/* *********************************************************************************************
+	 * Getters
+	 * *********************************************************************************************/
+	public Pixel[] getPixels() { return pixels; }
+
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+	
+	
 }
