8c60fb1fee528e71d55f21dca9063432a817bae7
Rex West <rexwest@gmail.com>
2015-09-17 08:27:26 +0900

    Added a simple ColorFog post-process PixelTransform

---
+++ src/raster/pixel/ColorFogPT.java
@@ -0,0 +1,48 @@
+package raster.pixel;
+
+import raster.Pixel;
+import raytrace.color.Color;
+
+public class ColorFogPT extends PixelTransform {
+	
+	/*
+	 * Applies a color fog
+	 */
+	
+	/* *********************************************************************************************
+	 * Instance Vars
+	 * *********************************************************************************************/
+	protected Color color;
+	protected double saturationDistance;
+	
+
+	/* *********************************************************************************************
+	 * Transform Override
+	 * *********************************************************************************************/
+	public ColorFogPT()
+	{
+		this.color = Color.white();
+		this.saturationDistance = Double.MAX_VALUE;
+	}
+	
+	public ColorFogPT(Color color, double saturationDistance)
+	{
+		this.color = color;
+		this.saturationDistance = saturationDistance;
+	}
+	
+
+	/* *********************************************************************************************
+	 * Transform Override
+	 * *********************************************************************************************/
+	@Override
+	public Color transform(Pixel pixel)
+	{
+		double opacity = Math.max(0.0, Math.min(pixel.getDepth() / saturationDistance, 1.0));
+		
+		Color result = Color.interpolate(pixel.getColor(), color, opacity);
+		
+		return result;
+	}
+
+}
