8c704eb28082f1d045c01d4b2c6969cdd247b179
Rex West <rexwest@gmail.com>
2015-09-17 08:33:48 +0900

    Updated PointLight to accept a Scene (for shadowing tests) and changed all instance of MAX_VALUE to POSITIVE_INFINITY

--- src/raytrace/light/PointLight.java
+++ src/raytrace/light/PointLight.java
@@ -4,7 +4,7 @@ import math.Vector3;
 import math.ray.Ray;
 import raytrace.data.IlluminationData;
 import raytrace.data.IntersectionData;
-import raytrace.data.ShadingData;
+import raytrace.scene.Scene;
 
 public class PointLight extends Light {
 	
@@ -16,7 +16,7 @@ public class PointLight extends Light {
 	 * *********************************************************************************************/
 
 	@Override
-	public IlluminationData illuminate(ShadingData data, Vector3 point)
+	public IlluminationData illuminate(Scene scene, Vector3 point)
 	{
 		IlluminationData ildata = new IlluminationData();
 		Vector3 toPoint = point.subtract(position);
@@ -28,7 +28,7 @@ public class PointLight extends Light {
 							 quadraticAttenuation * toPointMagSqrd;
 		
 		
-		IntersectionData shadowData = shadowed(data.getRootScene(), 
+		IntersectionData shadowData = shadowed(scene, 
 				new Ray(point, toPoint.multiply(-1.0), 0, 0), distance);
 		
 		double blocked = shadowData == null ? 1.0 : 0.0;
