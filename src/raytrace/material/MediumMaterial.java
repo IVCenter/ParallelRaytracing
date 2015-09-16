86886105793d13655943e0416c2e143c14c7aa8b
Rex West <rexwest@gmail.com>
2015-09-17 08:49:44 +0900

    Added MediumMaterial, a Material implementation that hands off incoming rays to the attached Medium.

---
+++ src/raytrace/material/MediumMaterial.java
@@ -0,0 +1,62 @@
+package raytrace.material;
+
+import math.Vector3;
+import raytrace.color.Color;
+import raytrace.data.IntersectionData;
+import raytrace.data.RayData;
+import raytrace.medium.Medium;
+
+public class MediumMaterial  extends Material{
+	
+	/*
+	 * An implementation of a material that hands off to a medium
+	 */
+	/* *********************************************************************************************
+	 * Instance Vars
+	 * *********************************************************************************************/
+	//
+	
+
+	/* *********************************************************************************************
+	 * Constructor
+	 * *********************************************************************************************/
+	public MediumMaterial(Medium medium)
+	{
+		this.medium = medium;
+		this.affectedByLightSources = false;
+	}
+	
+
+	/* *********************************************************************************************
+	 * Materail Overrides
+	 * *********************************************************************************************/
+	@Override
+	public RayData sample(IntersectionData idata, RayData rdata)
+	{
+		Vector3 point = idata.getPoint();
+		Vector3 direction = rdata.getRay().getDirection();
+		
+		RayData newRData = new RayData(point, direction, RayData.Type.TRASMIT);
+		
+		return newRData;
+	}
+
+	@Override
+	public Color evaluateDirectLight(IntersectionData idata, RayData rdata, Color light, Vector3 lightDirection)
+	{
+		return Color.black();
+	}
+
+	@Override
+	public Color evaluateSampledLight(IntersectionData idata, RayData rdata, Color light, RayData sample)
+	{
+		return light;
+	}
+
+	@Override
+	public Color evaluateEmission(IntersectionData idata, RayData rdata)
+	{
+		return Color.black();
+	}
+	
+}
