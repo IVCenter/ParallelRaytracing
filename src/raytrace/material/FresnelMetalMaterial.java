82c82593e50a7d18d7cb22fc40066f47f29a8320
Rex West <rexwest@gmail.com>
2015-09-17 08:47:59 +0900

    Updated the FresnelMetalMaterial to the new Materials interface (and fixed quite a few errors in the calculations :/ ).

--- src/raytrace/material/FresnelMetalMaterial.java
+++ src/raytrace/material/FresnelMetalMaterial.java
@@ -2,16 +2,16 @@ package raytrace.material;
 
 import math.Vector3;
 import raytrace.color.Color;
-import raytrace.data.IlluminationData;
-import raytrace.data.ShadingData;
-import raytrace.light.Light;
+import raytrace.data.IntersectionData;
+import raytrace.data.RayData;
 import raytrace.map.texture.Texture;
 
 public class FresnelMetalMaterial extends Material {
 	
 	/*
-	 * An implementation of a material that is a diffuse color and affected by light
+	 * An implementation of a fresnel metal material with n and k parameters
 	 */
+	
 	/* *********************************************************************************************
 	 * Instance Vars
 	 * *********************************************************************************************/
@@ -28,9 +28,123 @@ public class FresnelMetalMaterial extends Material {
 		this.tintTexture = tintTexture;
 		this.refractiveReal = refractiveReal;
 		this.refractiveImaginary = refractiveImaginary;
+		
+		this.affectedByLightSources = false;
+		this.emitsLight = false;
 	}
 	
 
+	/* *********************************************************************************************
+	 * Material Overrides
+	 * *********************************************************************************************/
+	@Override
+	public RayData sample(IntersectionData idata, RayData rdata)
+	{
+		Vector3 point = idata.getPoint();
+		Vector3 normal = idata.getNormal().normalizeM();
+		Vector3 ray = rdata.getRay().getDirection();
+		
+		double DdotN = normal.dot(ray);
+		
+		//If the normal is facing in the wrong direction, flip it
+		if(DdotN > 0.0) {
+			normal = normal.multiply(-1.0);
+		}
+		
+		//Calculate the reflective percent
+		double reflectivePercent = reflectivePercent(normal, ray);
+
+		//If reflective, go divin'
+		RayData newRData = new RayData(point, null, RayData.Type.REFLECT);
+		if(reflectivePercent != 0.0)
+		{
+			newRData.getRay().setDirection(reflect(ray, point, normal));
+			return newRData;
+		}
+		
+		return null;
+	}
+
+	@Override
+	public Color evaluateDirectLight(IntersectionData idata, RayData rdata, Color light, Vector3 lightDirection)
+	{
+		return Color.black();
+	}
+
+	@Override
+	public Color evaluateSampledLight(IntersectionData idata, RayData rdata, Color light, RayData sample)
+	{
+		//Get the material color from the texture
+		Color tint = tintTexture.evaluate(idata);
+		
+		Vector3 normal = idata.getNormal().normalizeM();
+		Vector3 ray = rdata.getRay().getDirection();
+		
+		double DdotN = normal.dot(ray.normalize());
+		//If the normal is facing in the wrong direction, flip it
+		if(DdotN > 0.0) {
+			normal = normal.multiply(-1.0);
+		}
+		
+		//Calculate the reflective percent
+		double baseReflectivePercent = reflectivePercent(normal, normal.multiply(-1.0));
+		double reflectivePercent = reflectivePercent(normal, ray);
+		double falloffReflection = 0.0;
+				
+		if(baseReflectivePercent < 1.0)
+			falloffReflection = (reflectivePercent - baseReflectivePercent) / (1.0 - baseReflectivePercent);
+		
+		if(falloffReflection < 0.0)
+			falloffReflection = 0.0;
+		
+		//If reflective, calculate how much light was not absorbed
+		Color unabsorbedLight;
+		if(reflectivePercent != 0.0)
+		{
+			unabsorbedLight = light.multiply3(reflectivePercent);
+		}else{
+			return Color.black();
+		}
+		
+		//Calculate the reflected light
+		Color reflectedLight = unabsorbedLight.multiply3M(tint.mixWithWhite(1.0 - falloffReflection, falloffReflection));
+		
+		return reflectedLight;
+	}
+
+	@Override
+	public Color evaluateEmission(IntersectionData idata, RayData rdata)
+	{
+		return Color.black();
+	}
+	
+	
+	/* *********************************************************************************************
+	 * Helper Methods
+	 * *********************************************************************************************/
+	protected double reflectivePercent(Vector3 normal, Vector3 ray)
+	{
+		//Calculate percent reflective
+		double NdotD = normal.dot(ray.normalize());
+		double NdotD2 = NdotD * NdotD;
+		double riReal2 = refractiveReal * refractiveReal;
+		double riImag2 = refractiveImaginary * refractiveImaginary;
+		double ri2Sum = (riReal2 + riImag2);
+		
+		double rpLeft = ri2Sum * NdotD2;
+		double rpRight = 2.0 * refractiveReal * NdotD;
+		
+		double reflParallel = (rpLeft + rpRight + 1.0) / (rpLeft - rpRight + 1.0);
+		double reflPerp = (ri2Sum + NdotD2 + rpRight) / (ri2Sum + NdotD2 - rpRight);
+
+		double reflectivePercent = 0.5 * (reflParallel * reflParallel + reflPerp * reflPerp);
+		reflectivePercent = reflectivePercent < 0.0 ? 0.0 : (reflectivePercent > 1.0 ? 1.0 : reflectivePercent);
+		
+		return reflectivePercent;
+	}
+	
+	
+	/*
 	@Override
 	public Color shade(ShadingData data)
 	{
@@ -42,19 +156,20 @@ public class FresnelMetalMaterial extends Material {
 		
 		Vector3 point = data.getIntersectionData().getPoint();
 		Vector3 normal = data.getIntersectionData().getNormal();
-		IlluminationData ildata;
+		Vector3 ray = data.getRay().getDirection();
 		
-		//Diffuse lighting and shadows
-		for(Light light : data.getRootScene().getLightManager())
-		{
-			//Get illumination data for the current light
-			ildata = light.illuminate(data, point);
-			
-			shade.add3M(diffuse(ildata.getColor(), normal, ildata.getDirection()));
+		
+		double DdotN = normal.dot(ray);
+		//If the normal is facing in the wrong direction, flip it
+		if(DdotN > 0.0) {
+			normal = normal.multiply(-1.0);
 		}
 		
+		
+		
+		
 		//Calculate percent reflective
-		double NdotD = normal.dot(data.getRay().getDirection());
+		double NdotD = normal.dot(ray);
 		double NdotD2 = NdotD * NdotD;
 		double riReal2 = refractiveReal * refractiveReal;
 		double riImag2 = refractiveImaginary * refractiveImaginary;
@@ -81,5 +196,7 @@ public class FresnelMetalMaterial extends Material {
 		return reflectiveColor;
 		
 	}
+	 */
+
 
 }
