dc944c70dfe2866772dd180608a047b15328f213
Rex West <rexwest@gmail.com>
2015-09-17 08:30:55 +0900

    Updated RayData with Type and SubType member variables.

--- src/raytrace/data/RayData.java
+++ src/raytrace/data/RayData.java
@@ -1,6 +1,7 @@
 package raytrace.data;
 
-import raytrace.surfaces.CompositeSurface;
+import system.Constants;
+import math.Vector3;
 import math.ray.Ray;
 
 public class RayData {
@@ -8,12 +9,19 @@ public class RayData {
 	/*
 	 * Used for passing ray casting data down the recursive call chain
 	 */
+	
+	/* *********************************************************************************************
+	 * Enums
+	 * *********************************************************************************************/
+	public enum Type {REFLECT, TRASMIT, NONE};
+	
+	
 	/* *********************************************************************************************
 	 * Instance Vars
 	 * *********************************************************************************************/
 	protected Ray ray;
-	protected CompositeSurface rootSurface;
-	//protected IntersectionData idata;
+	protected Type type = Type.NONE;
+	protected int subType = -1;
 
 	protected double t0;
 	protected double t1;
@@ -25,25 +33,35 @@ public class RayData {
 	public RayData()
 	{
 		t0 = 0;
-		t1 = Double.MAX_VALUE;
+		t1 = Double.POSITIVE_INFINITY;
 	}
 	
-	public RayData(Ray ray, CompositeSurface rootSurface/*, IntersectionData idata*/)
+	public RayData(Ray ray)
 	{
 		this();
 		this.ray = ray;
-		this.rootSurface = rootSurface;
-		//this.idata = idata;
 	}
 	
-	public RayData(Ray ray, CompositeSurface rootSurface/*, IntersectionData idata*/, double t0, double t1)
+	public RayData(Ray ray, double t0, double t1)
 	{
 		this();
 		this.t0 = t0;
 		this.t1 = t1;
 		this.ray = ray;
-		this.rootSurface = rootSurface;
-		//this.idata = idata;
+	}
+	
+	public RayData(Vector3 origin, Vector3 direction, Type type)
+	{
+		this();
+		this.t0 = Constants.RECURSIVE_EPSILON;
+		this.ray = new Ray(origin, direction, 0, 0);
+		this.type = type;
+	}
+	
+	public RayData(Vector3 origin, Vector3 direction, Type type, int subType)
+	{
+		this(origin, direction, type);
+		this.subType = subType;
 	}
 	
 
@@ -53,7 +71,7 @@ public class RayData {
 	public void clear()
 	{
 		t0 = 0;
-		t1 = Double.MAX_VALUE;
+		t1 = Double.POSITIVE_INFINITY;
 	}
 	
 
@@ -67,23 +85,7 @@ public class RayData {
 	public void setRay(Ray ray) {
 		this.ray = ray;
 	}
-
-	public CompositeSurface getRootSurface() {
-		return rootSurface;
-	}
-
-	public void setRootSurface(CompositeSurface rootSurface) {
-		this.rootSurface = rootSurface;
-	}
-	/*
-	public IntersectionData getIdata() {
-		return idata;
-	}
-
-	public void setIdata(IntersectionData idata) {
-		this.idata = idata;
-	}
-	 */
+	
 	public double getTStart() {
 		return t0;
 	}
@@ -98,5 +100,21 @@ public class RayData {
 
 	public void setTEnd(double t1) {
 		this.t1 = t1;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+	public int getSubType() {
+		return subType;
+	}
+
+	public void setSubType(int subType) {
+		this.subType = subType;
 	}
 }
