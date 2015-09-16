a5a02b2018416b514a34affa0609dbcb95c17e42
Rex West <rexwest@gmail.com>
2015-09-17 08:47:00 +0900

    Updated the ColorMaterial to the new Materials interface.

--- src/raytrace/material/ColorMaterial.java
+++ src/raytrace/material/ColorMaterial.java
@@ -1,7 +1,9 @@
 package raytrace.material;
 
+import math.Vector3;
 import raytrace.color.Color;
-import raytrace.data.ShadingData;
+import raytrace.data.IntersectionData;
+import raytrace.data.RayData;
 import raytrace.map.texture.Texture;
 
 public class ColorMaterial extends Material {
@@ -9,6 +11,7 @@ public class ColorMaterial extends Material {
 	/*
 	 * An implementation of a material that is a diffuse color
 	 */
+	
 	/* *********************************************************************************************
 	 * Instance Vars
 	 * *********************************************************************************************/
@@ -21,17 +24,48 @@ public class ColorMaterial extends Material {
 	public ColorMaterial(Texture colorTexture)
 	{
 		this.colorTexture = colorTexture;
+		this.globallyIlluminated = false;
+		this.affectedByLightSources = false;
+		this.emitsLight = true;
 	}
 	
 
+	/* *********************************************************************************************
+	 * Material Overrides
+	 * *********************************************************************************************/
 	@Override
-	public Color shade(ShadingData data)
+	public RayData sample(IntersectionData idata, RayData rdata)
 	{
-		return colorTexture.evaluate(data.getIntersectionData());
+		return null;
 	}
-	
-	public void setTexture(Texture colorTexture)
+
+	@Override
+	public Color evaluateDirectLight(IntersectionData idata, RayData rdata, Color light, Vector3 lightDirection)
 	{
+		return Color.black();
+	}
+
+	@Override
+	public Color evaluateSampledLight(IntersectionData idata, RayData rdata, Color light, RayData sample)
+	{
+		return Color.black();
+	}
+
+	@Override
+	public Color evaluateEmission(IntersectionData idata, RayData rdata)
+	{
+		return colorTexture.evaluate(idata);
+	}
+
+
+	/* *********************************************************************************************
+	 * Getters/Setters
+	 * *********************************************************************************************/
+	public Texture getColorTexture() {
+		return colorTexture;
+	}
+
+	public void setColorTexture(Texture colorTexture) {
 		this.colorTexture = colorTexture;
 	}
 
